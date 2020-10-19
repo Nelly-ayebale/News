31750aab3e1bf4eae6117bdc90e69f2c917cb26e
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-19 07:54:35 +0300

    Updated the test module

---
+++ tests/test_news.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setup(self):
+        '''
+        Setup method that will run before each test
+        '''
+        self.new_news = News('abc', 'ABC NEWS','The best news worldwide','www.abc.com', 'general', 'UK')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news, News))
+        
