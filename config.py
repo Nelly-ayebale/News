217561b99c8849e1f1c690b569f70bb0197c7e9e
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-19 07:21:07 +0300

    Initialized the app and created a blueprint

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+
+    NEWS_SOURCE_BASE_URL ='https://newsapi.org/v2/sources?category{}&api_key={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
