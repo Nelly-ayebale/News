bbc726b7a60c43b5042a354b6203d7e37d0434c6
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-19 11:09:19 +0300

    Created a Url for the Articles

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 
     NEWS_SOURCE_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     
 
 
