418ab7a87e71469065ea6e6dbf7529d910c63272
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-19 07:41:02 +0300

    Updated the requests.py

---
+++ app/requests.py
@@ -0,0 +1,46 @@
+import urllib.request,json
+from .models import News
+
+api_key = None
+news_url = None
+
+def configure_request(app):
+    global api_key, news_url
+    api_key = app.config['NEWS_API_KEY']
+    news_url = app.config['NEWS_SOURCE_BASE_URL']
+
+def get_news(category):
+    '''
+    Function that gets the json response to the url request
+    '''
+    get_news_url = news_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function that processes the news_result anf returns a list of objects
+    '''
+
+    news_results=[]
+    for news_item in news_list:
+        id = news_item.get('id')
+        name= news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        country = news_item.get('country')
+
+        news_object= News(id,name,description, url, category, country)
+        news_results.append(news_object)
+    
+    return news_results
